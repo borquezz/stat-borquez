eeedb532ecd01c8e2418cdca009282863073a8fb
borquezz <jesusfernandoborquez@gmail.com>
2021-10-14 23:06:27 +0900

    mean2, med2 and pareto published

--- lib.js
+++ lib.js
@@ -1,3 +1,12 @@
+function divide2d(numbers) {
+    let x = [],
+        y = [];
+    for (let i = 0; i < numbers.length; i += 2) {
+        x.push(numbers[i]);
+        y.push(numbers[i + 1]);
+    }
+    return [x, y];
+}
 function sum(numbers) {
     return numbers.reduce((prev, curr) => prev + curr, 0);
 }
@@ -10,8 +19,58 @@ function max(numbers) {
     return numbers.reduce((max, curr) => (max > curr ? max : curr), numbers[0]);
 }
 
+function mean2(numbers) {
+    const points = divide2d(numbers);
+    let mean = [avg(points[0]), avg(points[1])];
+    return mean;
+}
+
+function med2(numbers) {
+    const points = divide2d(numbers);
+    let x = points[0],
+        y = points[1];
+
+    let minIndex = 0,
+        min = Infinity;
+    for (let i = 0; i < x.length; i++) {
+        let count = 0;
+        for (let j = 0; j < y.length; j++) {
+            count += Math.sqrt(
+                Math.pow(x[i] - x[j], 2) + Math.pow(y[i] - y[j], 2)
+            );
+        }
+        if (count < min) {
+            min = count;
+            minIndex = i;
+        }
+    }
+    return [x[minIndex], y[minIndex]];
+}
+
+function pareto(numbers) {
+    const points = divide2d(numbers);
+    let x = points[0],
+        y = points[1];
+
+    let paretos = [];
+    for (let i = 0; i < x.length; i++) {
+        let flag = 0;
+        for (let j = 0; j < y.length; j++) {
+            if (x[i] < x[j] && y[i] < y[j]) {
+                flag = 1;
+                break;
+            }
+        }
+        if (!flag) paretos.push([x[i], y[i]]);
+    }
+    return paretos;
+}
+
 module.exports = {
     sum,
     avg,
     max,
+    mean2,
+    med2,
+    pareto,
 };
--- main.js
+++ main.js
@@ -24,16 +24,31 @@ let result = 0;
 switch (command) {
     case "sum":
         result = lib.sum(numbers);
+        console.log(result);
         break;
     case "avg":
         result = lib.avg(numbers);
+        console.log(result);
         break;
     case "max":
         result = lib.max(numbers);
+        console.log(result);
+        break;
+    case "mean2":
+        result = lib.mean2(numbers);
+        console.log(result[0].toFixed(2), result[1].toFixed(2)); //Prints them as strings, ask if its okay
+        break;
+    case "med2":
+        result = lib.med2(numbers);
+        console.log(result[0].toFixed(2), result[1].toFixed(2));
+        break;
+    case "pareto":
+        result = lib.pareto(numbers);
+        for (let i = 0; i < result.length; i++) {
+            console.log(result[i][0], result[i][1]);
+        }
         break;
     default:
         console.log("Wrong command!");
         process.exit(1);
 }
-
-console.log(result);
